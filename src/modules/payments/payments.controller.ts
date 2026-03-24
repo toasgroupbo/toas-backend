c6688aac5301c5adc7690445dcc3c875d955ca1a
Diego Borja <luisdiegoborja8@gmail.com>
2026-03-24 09:39:34 -0400

    feat: añadir logs para verificar error

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -71,8 +71,10 @@ export class PaymentsController {
   @UseGuards(BasicAuthGuard)
   async receiveQrCallback(@Body() response: QrCallbackResponse) {
     try {
-      await this.paymentsService.callback(response);
+      //await this.paymentsService.callback(response);
       const id = response.Id;
+
+      console.log(response);
 
       return {
         State: '000',
