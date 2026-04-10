bb2527c744474707b39d09e63adb00c73697f94c
Diego Borja <luisdiegoborja8@gmail.com>
2026-04-10 15:39:33 -0400

    feat: añadir actualizacon de cuentas de banco en company y owners

--- src/modules/owners/dto/update-owner.dto.ts
+++ src/modules/owners/dto/update-owner.dto.ts
@@ -2,5 +2,5 @@ import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateOwnerDto } from './create-owner.dto';
 
 export class UpdateOwnerDto extends PartialType(
-  OmitType(CreateOwnerDto, ['bankAccount'] as const),
+  CreateOwnerDto, //OmitType(CreateOwnerDto, ['bankAccount'] as const),
 ) {}
