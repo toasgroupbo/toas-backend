ffc8a91cee66e1b2e62c2ccda7e0d96337ab5bce
Diego Borja <luisdiegoborja8@gmail.com>
2026-04-24 12:17:01 -0400

    fix: arreglo en findAll de tickets para admins

--- src/modules/tickets/pagination/ticket-for-cashier-pagination.dto.ts
+++ src/modules/tickets/pagination/ticket-for-cashier-pagination.dto.ts
@@ -1,9 +1,15 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional } from 'class-validator';
 
 import { TicketStatus } from '../enums';
+import { Type } from 'class-transformer';
 
 export class TicketForCashierFilterDto {
   @IsOptional()
   @IsEnum([TicketStatus.SOLD, TicketStatus.CANCELLED])
   status?: TicketStatus;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  companyId?: number;
 }
